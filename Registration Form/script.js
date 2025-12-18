c54ffbb571cd341de6878c791df949a327618cab
Narmada-Natarajan <narmadanatarajan3629@gmail.com>
2025-12-18 23:09:20 +0530

    [Added]: Registration Form

---
+++ Registration Form/script.js
@@ -0,0 +1,24 @@
+const form=document.getElementById("login-form")
+console.log(form)
+
+form.addEventListener("submit",(event)=>{
+    const Firstname=document.getElementById("first-name").value;
+    const Lastname=document.getElementById("last-name").value;
+    const Emailid=document.getElementById("mail-id").value;
+    const Dateofbirth=document.getElementById("your-dob").value;
+    const Password=document.getElementById("pass").value;
+    const Confirmpassword=document.getElementById("confirm-pass").value;
+    event.preventDefault();
+
+
+
+
+    if(
+
+
+        
+    )
+
+
+});
+
